4d83bed6d9cd2be11b27812e052b561edc31f8c4
ndl10 <159472389+ndl10@users.noreply.github.com>
2024-03-29 21:18:26 +0000

    proyect ndl10

--- src/js/app.js
+++ src/js/app.js
@@ -31,19 +31,35 @@ function render(variables = {}) {
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+      ${cover}
+      <img src="${variables.avatarURL}" class="photo" />
+      <h1>${variables.name} ${variables.lastName}</h1>
+      <h2>${variables.role}</h2>
+      <h3>${variables.city} ${variables.country}</h3>
+      <ul class="${variables.socialMediaPosition}">
+        <li>
+          <a href="https://twitter.com/4geeksacademy">
+            <i class="fab fa-twitter"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/4geeksacademy">
+            <i class="fab fa-github"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://linkedin.com/school/4geeksacademy">
+            <i class="fab fa-linkedin"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://instagram.com/4geeksacademy">
+            <i class="fab fa-instagram"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  `;
 }
 
 /**
@@ -56,7 +72,8 @@ window.onload = function() {
     // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/como-hacer-a-tu-gato-feliz.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
